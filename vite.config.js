d5328394229201ef9ad1378c140874597e708247
pencil <yuyu.hunter@gmail.com>
2024-12-29 23:59:41 +0800

    完成Vite環境建置影音課程內容

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 判斷開發中，還是產品(production)的路徑
+  base: process.env.NODE_ENV === 'production' ? '/202412_vite-project/' : '/',
   plugins: [react()],
 })
